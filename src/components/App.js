ceae83236fd00f809984470e9e90848b411a1b4f
donell <donell.wambua@student.moringaschool.com>
2023-07-20 11:42:54 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,25 @@ import React from "react";
 import Container from "./Container";
 
 function App() {
+  const exampleContent = Array.from({ length: 7 }, (_, index) => (
+    <div key={index}>This is an example!</div>
+  ));
+
+  const exampleImages = Array.from({ length: 4 }, (_, index) => (
+    <img
+      key={index}
+      alt="turtle"
+      src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+    />
+  ));
+
   return (
     <div className="App">
       <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+        {exampleContent}
       </Container>
       <Container header="Example header" textPosition="center">
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+        {exampleImages}
       </Container>
     </div>
   );
